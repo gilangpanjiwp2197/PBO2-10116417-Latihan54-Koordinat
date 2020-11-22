9f33149e464c531da9cce2349ae56a582c236dd9
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-22 12:35:03 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan54-Koordinat/src/pbo2/pkg10116417/latihan54/koordinat/PBO210116417Latihan54Koordinat.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan54.koordinat;
+
+/**
+ *
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM WARNA KOORDINAT
+ */
+public class PBO210116417Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat wk = new  WarnaKoordinat(0,0,"");
+        wk.setNamaWarna("Jingga");
+        wk.setX(10);
+        wk.setY(4);
+        System.out.println("Warna Koordinat : " + wk.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + wk.getX() + ", y : " 
+                            + wk.getY());
+        
+    }
+    
+}
---
+++ PBO2-10116417-Latihan54-Koordinat/src/pbo2/pkg10116417/latihan54/koordinat/WarnaKoordinat.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan54.koordinat;
+
+/**
+ *
+ * 
+ */
+public class WarnaKoordinat extends Koordinat {
+    public String namaWarna;
+
+    public WarnaKoordinat(int x, int y, String namaWarna){
+     super (x, y);
+    }
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+    
+}
